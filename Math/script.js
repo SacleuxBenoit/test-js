b92de659da18b8f552987f3f159ad42b2aa6ecb4
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-03-27 23:29:36 +0100

    add a test for Math.floor and Math.random in script.js

---
+++ Math/script.js
@@ -0,0 +1,9 @@
+// Test d'utilisation avec Math() pour mes cheat sheets
+
+// Math.floor et Math.random : 
+
+function entierAleatoire(min,max){
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+const entier = entierAleatoire(1,10);
+console.log(entier)
