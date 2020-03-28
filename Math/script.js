779c706eaa46844c15da511fa82cddc623893e52
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-03-28 17:53:13 +0100

    add some comment at line 9,11 and 14 in script.js

--- Math/script.js
+++ Math/script.js
@@ -6,10 +6,9 @@ function entierAleatoire(min,max){
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 const entier = entierAleatoire(1,10);
-console.log(entier)
-
+console.log(entier) // Affiche un nombre aléatoire entre 1 et 10
 // Math.min()
-console.log(Math.min(-3, 2, 9))
+console.log(Math.min(-3, 2, 9)) // Affiche -3 dans la console 
 
 // Math.max()
-console.log(Math.max(4,919,3))
+console.log(Math.max(4,919,3)) // Affiche 919 dans la console 
