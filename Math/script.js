010e4c3fadfc976850106a98d84db67368e340a0
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-03-28 18:01:55 +0100

    add exemple for math.min and math.max in script.js

--- Math/script.js
+++ Math/script.js
@@ -10,5 +10,11 @@ console.log(entier) // Affiche un nombre aléatoire entre 1 et 10
 // Math.min()
 console.log(Math.min(-3, 2, 9)) // Affiche -3 dans la console 
 
+const array = [-4, 5, -3, 15]
+console.log(Math.min(...array)); // Affiche -4 dans la console 
+
 // Math.max()
 console.log(Math.max(4,919,3)) // Affiche 919 dans la console 
+
+const arr = [-4, 5, -3, 15]
+console.log(Math.max(...arr)); // Affiche 15 dans la console 
