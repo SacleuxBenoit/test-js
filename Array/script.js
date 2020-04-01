3975565fe56744113f970450c4d88b31d697be22
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-04-01 16:35:39 +0200

    add the part (Voir la taille du tableau, Acceder à un element du tableau and Acceder au dernier element du tableau) in script.js

---
+++ Array/script.js
@@ -0,0 +1,17 @@
+// Test sur les arrays pour mes cheat sheets
+
+// --------------- Voir la taille du tableau --------------- 
+
+const number = [2, 2, 10, 287]
+
+console.log(number.length) // Affiche 4
+
+// --------------- Accéder à un element du tableau --------------- 
+
+const elem = [3, 9, 7, 279]
+console.log(elem[1]) // Affiche 9
+
+// --------------- Accéder au dernier element du tableau --------------- 
+
+const last = [3, 8, 238, 278]
+console.log(last[last.length-1]) // Affiche 278
