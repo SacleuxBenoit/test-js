86b2c4a7781a6aa5e4709e16c889326c5cf4d087
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-04-20 13:40:36 +0200

    add the part (Exemple de try...catch qui fonctionne) in script.js

---
+++ Try...catch/script.js
@@ -0,0 +1,9 @@
+// Test sur Try...catch pour mes cheat sheets
+
+// Exemple de try... catch qui fonctionne
+
+try{
+    console.log(Math.random(1,10))
+}catch(err){
+    console.log("Math.random is not working")
+}
