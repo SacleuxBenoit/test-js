c0c95da1eee96752aecfde8baa6562e342262615
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-04-23 16:12:55 +0200

    add another exemple of a wrong code in script.js

--- Try...catch/script.js
+++ Try...catch/script.js
@@ -15,3 +15,15 @@ try{
 }catch(err){
     console.log("Math.random is not working")
 }
+
+// autre exemple de code qui ne fonctionne pas avec Try...catch
+
+try {
+    rand; // error, variable is not defined!
+  } catch(err) {
+    console.log(err.name); // ReferenceError
+    console.log(err.message); // rand is not defined
+    console.log(err.stack); // ReferenceError: rand is not defined at (...call stack)
+    // The error is converted to string as "name: message"
+    console.log(err); // ReferenceError: rand is not defined
+  }
