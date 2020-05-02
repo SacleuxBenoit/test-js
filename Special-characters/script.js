d786380e41b73eed1e9eb7471a88ed4976edaced
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-05-02 15:06:27 +0200

    correction of misspelling in script.js of Special-characters

--- Special-characters/script.js
+++ Special-characters/script.js
@@ -7,7 +7,7 @@
 let orcs = 'Je suis un orc Mag'har';
 console.log(orcs) // SyntaxError: Unexpected identifier 'har'. Expected ';' after variable declaration.
 
-// Sans d'erreur
+// Sans erreur
 
 let orc = 'Je suis un orc Mag\'har'
 console.log(orc) // Je suis un orc Mag'har
@@ -19,7 +19,7 @@ console.log(orc) // Je suis un orc Mag'har
 let herboriste = "Je ramasse de la zin"anthide";
 console.log(herboriste) // SyntaxError: Unexpected identifier 'anthide'. Expected ';' after variable declaration.
 
-// Sans d'erreur
+// Sans erreur
 
 let herbo = "Je ramasse de la zin\"anthide"
 console.log(herbo) // Je ramasse de la zin"anthide
