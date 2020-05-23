7c0dd43fd45a0c7487febe90da7db869c8add771
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-05-23 23:16:51 +0200

    add a function for a conversion of miles to km in script.js of other-test

---
+++ other-Test/script.js
@@ -0,0 +1,7 @@
+function conversion(){
+    let message= prompt("Entrez la valeur en miles");
+    let resultat = message * 1.60934;
+    alert(message + " miles est égal à " + resultat + " Km")
+}
+
+conversion()
