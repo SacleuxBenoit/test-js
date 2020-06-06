60d17e5c6881173883537094619335bc1deb554b
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-06-06 14:23:07 +0200

    add a test with Switch for .keycode in script.js of Keyboard-Event

--- Keyboard-Event/script.js
+++ Keyboard-Event/script.js
@@ -24,10 +24,30 @@ message.addEventListener('keydown', (e) => {
     }
 })
 
-// utilisation de keyup avec .keyCOde
+// Utilisation de keyup avec .keyCode
 
 message.addEventListener('keyup', (e) =>{
     if(e.keyCode == "65"){
         console.log("A à était relaché")
+    }
+})
+
+// Utilisation de switch avec .keyCode
+message.addEventListener('keydown', (e) =>{
+    switch(e.keyCode){
+        case 65:
+            console.log("C'est un A");
+            break;
+        case 66:
+            console.log("C'est un B");
+            break;
+        case 67:
+            console.log("C'est un C");
+            break;
+        case 82:
+            console.log("C'est un R")
+            break;
+        default:
+            console.log("La touche n'a pas encore était codé")
     }
 })
