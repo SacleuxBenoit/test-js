4f7fb08aa579ad86dc850164d9aca62248f6ce61
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-06-06 13:33:09 +0200

    add a test with .keyCode for keydown in script.js of Keyboard-Event

--- Keyboard-Event/script.js
+++ Keyboard-Event/script.js
@@ -1,13 +1,25 @@
 let message = document.getElementById('TextArea')
 
+// Utilisation de keydown avec .key
+
 message.addEventListener('keydown', (e) => {
     if(e.key == "z"){
         console.log("Z à était press")
     }
 })
 
+// Utilisation de keyup avec .key
+
 message.addEventListener('keyup', (e) => {
     if(e.key == "z"){
         console.log("Z à était relaché")
+    }
+})
+
+// Utilisation de keydown avec .keyCode
+
+message.addEventListener('keydown', (e) => {
+    if(e.keyCode == "65"){
+        console.log("A à était press")
     }
 })
