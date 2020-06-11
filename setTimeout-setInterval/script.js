105eda79759eb0f796cf20b5ddcd01d351dd0e16
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-06-11 21:50:25 +0200

    add a test with clearInterval in script.js of setTimeout-setInterval

--- setTimeout-setInterval/script.js
+++ setTimeout-setInterval/script.js
@@ -1,3 +1,15 @@
-setInterval(() =>{
+/*setInterval(() =>{
     alert("This is a test with 3000 ms")
-}, 3000)
+}, 3000)*/
+
+// ClearInterval
+
+const content;
+
+function sayHello(){
+    content = setTimeout(function(){alert("Hi")}, 4000);
+}
+
+function dontSayHello(){
+    clearTimeout(content)
+}
