c95b5f1dc593913936d264e32b98bbda3fc2e52f
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-06-22 16:58:34 +0200

    add some test for multiword in script.js of Objects

--- Objects/script.js
+++ Objects/script.js
@@ -2,12 +2,18 @@ let Orc = {
     arms : 2,
     legs : 2,
     skill : "unknow",
-    "class et race" : "unkwnow"
+    "class and spec" : "unknow"
 }
 
-console.log(Orc) // Ici Orc affiche {arms: 2, legs: 2, skill: "unknow", class et race: "unkwnow"}
+console.log(Orc) // Ici Orc affiche {arms: 2, legs: 2, skill: "unknow", class and spec: "unkwnow"}
 
 Orc.Horde = true // Ajout Horde : true pour l'objet Orc
 delete Orc.skill // Supprime skill : "unknow" pour l'objet Orc
 
-console.log(Orc) // Et ici Orc affiche {arms: 2, legs: 2, class et race: "unkwnow", Horde: true}
+console.log(Orc) // Et ici Orc affiche {arms: 2, legs: 2, class and spec: "unkwnow", Horde: true}
+
+Orc["class and spec"] = "Hunter BM"
+console.log(Orc) // {arms: 2, legs: 2, class and spec: "Hunter BM", Horde: true}
+
+delete Orc["class and spec"]
+console.log(Orc) // {arms: 2, legs: 2, Horde: true}
