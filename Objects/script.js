999f6bbaca230a2f2ef075985076d12699950572
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-06-23 18:36:25 +0200

    add some test in script.js of Objects

--- Objects/script.js
+++ Objects/script.js
@@ -17,3 +17,41 @@ console.log(Orc) // {arms: 2, legs: 2, class and spec: "Hunter BM", Horde: true}
 
 delete Orc["class and spec"]
 console.log(Orc) // {arms: 2, legs: 2, Horde: true}
+
+let race = {
+    Orc : {
+        arms : 2,
+    },
+    Human : {
+        arms : 2,
+    },
+    Undead : {
+        arms : 1,
+    },
+
+}
+
+console.log(race.Orc.arms)
+console.log(race.Undead.arms)
+
+// Copy 
+
+let Human = {
+    name : "name",
+    age : "age",
+}
+let Race = Human
+Human.test = "test";
+
+console.log(Human); // affiche {name: "name", age: "age", test: "test"}
+console.log(Race); // affiche {name: "name", age: "age", test: "test"}
+
+let a = {};
+let b = {};
+
+console.log( a == b) // false
+
+let c = {};
+let d = c;
+
+console.log( c == d ) // true
