788c6ef49c68957bf44a302cf716ae117350c5aa
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-07-01 23:04:23 +0200

    add some test with the DOM in scrip.js

---
+++ DOM/script.js
@@ -0,0 +1,8 @@
+let loremId = document.getElementById("firstLoremId");
+let testParent = document.getElementById("testParent");
+let divs = document.getElementsByTagName('div')
+
+testParent.style.color="blue"
+loremId.style.color = "purple";
+
+alert(divs.length)
