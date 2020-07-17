73cb2fa2005eaa4e3a48f73effcb5222f8680df8
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-07-17 14:21:24 +0200

    delete the function Num(), correction of a problem in the while() and add the part else if(answer == random) in script.js

--- Le-juste-nombre/script.js
+++ Le-juste-nombre/script.js
@@ -1,33 +1,9 @@
-/*function Num(){
-    let valueInput = document.getElementById("valueInput").value
-    let result = document.getElementById("result")
-    if(valueInput == random){
-        alert("c'est good")
-    }else{alert("c'est pas good")}
-}*/
-
 let random = Math.floor(Math.random() * 1000);
 console.log(random)
 
 let question = "Entrez votre Number"
 let answer;
 let i = 0;
-
-/*do {
-    answer = prompt(question);
-
-    if (answer < random) {
-        alert("Votre nombres est inférieur");
-        i++
-    }
-
-    else {
-        alert("Votre nombre est supérieur");
-        i++
-    }
-
-} while (answer != random)
-*/
 
 while(answer != random){
 answer = prompt(question);
@@ -37,9 +13,14 @@ if (answer < random) {
     i++
 }
 
-else {
+else if(answer > random){
     alert("Votre nombre est supérieur");
     i++
 }
 
+else if (answer == random){
+    alert("Vous avez gagné en " + i + " coups")
 }
+
+}
+
