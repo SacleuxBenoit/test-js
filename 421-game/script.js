33e4ea65c4184167d236f8c0fb54288604be34a6
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-07-20 13:39:43 +0200

    add the function newArray with newarr in it in script.js

--- 421-game/script.js
+++ 421-game/script.js
@@ -5,48 +5,53 @@ let bigThrow = document.getElementById("bigThrow");
 let result = document.getElementById("result");
 
 function RandomBtn1() {
-    arrBtn1 = [Math.floor((Math.random() * 6) + 1)]
+    arrBtn1 = [Math.floor((Math.random() * 6) + 1)];
     console.log(arrBtn1)
 }
 function RandomBtn2() {
-    arrBtn2 = [Math.floor((Math.random() * 6) + 1)]
+    arrBtn2 = [Math.floor((Math.random() * 6) + 1)];
     console.log(arrBtn2)
 }
 function RandomBtn3() {
-    arrBtn3 = [Math.floor((Math.random() * 6) + 1)]
+    arrBtn3 = [Math.floor((Math.random() * 6) + 1)];
     console.log(arrBtn3)
 }
 
 function everyThing(){
     RandomBtn1();
     RandomBtn2();
-    RandomBtn3()
-    console.log(newarr = [...arrBtn1,...arrBtn2,...arrBtn3,])
+    RandomBtn3();
     sort();
     winner();
     disableButton();
     showRandom();
+    newArray();
 }
 
+function newArray(){
+    console.log(newarr = /*[...arrBtn1,...arrBtn2,...arrBtn3,]*/ arrBtn1.concat(arrBtn2, arrBtn3));
+}
 function sort(){
 
+    newArray();
+    
     if(newarr[1] == 4){
-        [newarr[0], newarr[1]] = [newarr[1], newarr[0]]
+        [newarr[0], newarr[1]] = [newarr[1], newarr[0]];
         console.log(newarr)
     }
 
     if(newarr[2] == 4){
-        [newarr[0], newarr[2]] = [newarr[2], newarr[0]]
+        [newarr[0], newarr[2]] = [newarr[2], newarr[0]];
         console.log(newarr)
     }
 
     if(newarr[0] == 2){
-        [newarr[0], newarr[1]] = [newarr[1], newarr[0]]
+        [newarr[0], newarr[1]] = [newarr[1], newarr[0]];
         console.log(newarr)
     }
 
     if(newarr[2] == 2){
-        [newarr[1], newarr[2]] = [newarr[2], newarr[1]]
+        [newarr[1], newarr[2]] = [newarr[2], newarr[1]];
         console.log(newarr)
     }
 }
@@ -96,5 +101,5 @@ function reset(){
     firstThrow.disabled = false;
     secondThrow.disabled = false;
     thirdThrow.disabled = false;
-    bigThrow.disabled = false
+    bigThrow.disabled = false;
 }
