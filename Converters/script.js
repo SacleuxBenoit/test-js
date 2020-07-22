52de11a7e72c22d5e2a630b936c80bfc81783341
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-07-22 11:15:14 +0200

    add function getKm in script.js of Converters

--- Converters/script.js
+++ Converters/script.js
@@ -1,6 +1,11 @@
 function getMiles(){
-   let KmToMiles= document.getElementById("KmToMiles").value
-   let resultMiles= document.getElementById("resultMiles")
+   let KmToMiles = document.getElementById("KmToMiles").value
+   let resultMiles = document.getElementById("resultMiles")
     KmToMiles > 0 ? resultMiles.textContent =  KmToMiles * 1.60934 + " Miles" : resultMiles.textContent = " "
 }
 
+function getKm(resultKm){
+    let MilesToKm = document.getElementById("MilesToKm").value
+     resultKm = document.getElementById("resultKm")
+    MilesToKm > 0 ? resultKm.textContent = MilesToKm / 1.60934 + " Km" : resultKm.textContent = " " 
+}
