05a2c7d3afbab25b6208caf94595adaf99a8e59e
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-07-22 12:27:40 +0200

    add function getCelsius() and getFahrenheit() in script.js of Converters

--- Converters/script.js
+++ Converters/script.js
@@ -4,8 +4,20 @@ function getMiles(){
     KmToMiles > 0 ? resultMiles.textContent =  KmToMiles * 1.60934 + " Miles" : resultMiles.textContent = " "
 }
 
-function getKm(resultKm){
+function getKm(){
     let MilesToKm = document.getElementById("MilesToKm").value
      resultKm = document.getElementById("resultKm")
     MilesToKm > 0 ? resultKm.textContent = MilesToKm / 1.60934 + " Km" : resultKm.textContent = " " 
+}
+
+function getCelsius(){
+    let FtoC = document.getElementById("FtoC").value
+    resultCelsius = document.getElementById("resultCelsius")
+    resultCelsius.textContent = (FtoC - 32) / 1.8 + " degré Celsuis"
+}
+
+function getFahrenheit(){
+    let CtoF = document.getElementById("CtoF").value
+    resultFahrenheit = document.getElementById("resultFahrenheit")
+    resultFahrenheit.textContent = CtoF * 1.8 + 32
 }
