7b4289e727419b7f067ba43bd6593eca53e41d39
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-07-22 14:33:30 +0200

    fixed issue with formula to convert to fahrenheit + add some string in the text.Content of each function in script.js of Converters

--- Converters/script.js
+++ Converters/script.js
@@ -1,23 +1,23 @@
 function getMiles(){
    let KmToMiles = document.getElementById("KmToMiles").value
    let resultMiles = document.getElementById("resultMiles")
-    KmToMiles > 0 ? resultMiles.textContent =  KmToMiles * 1.60934 + " Miles" : resultMiles.textContent = " "
+    KmToMiles > 0 ? resultMiles.textContent = KmToMiles  +" Km équivaut à " + KmToMiles * 1.60934 + " Miles" : resultMiles.textContent = " "
 }
 
 function getKm(){
     let MilesToKm = document.getElementById("MilesToKm").value
      resultKm = document.getElementById("resultKm")
-    MilesToKm > 0 ? resultKm.textContent = MilesToKm / 1.60934 + " Km" : resultKm.textContent = " " 
+    MilesToKm > 0 ? resultKm.textContent = MilesToKm + " Miles équivaut à " + MilesToKm / 1.60934 + " Km" : resultKm.textContent = " " 
 }
 
 function getCelsius(){
     let FtoC = document.getElementById("FtoC").value
     resultCelsius = document.getElementById("resultCelsius")
-    resultCelsius.textContent = (FtoC - 32) / 1.8 + " degré Celsuis"
+    resultCelsius.textContent = FtoC + " degrés Fahrenheit équivaut à " + (FtoC - 32) / 1.8 + " degrés Celsuis"
 }
 
 function getFahrenheit(){
     let CtoF = document.getElementById("CtoF").value
     resultFahrenheit = document.getElementById("resultFahrenheit")
-    resultFahrenheit.textContent = CtoF * 1.8 + 32
+    resultFahrenheit.textContent = CtoF + " degrés Celsius équivaut à " + (CtoF * 1.8 + 32)  + " degrés Fahrenheit"
 }
