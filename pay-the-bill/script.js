238c43343af1fe814447613ea65befa3d256c4ae
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-08-03 15:50:05 +0200

    delete the function calcTVA, instead put everything in the function send() and add the var totalPeople ( divide the total price with the number of people) in script.js

--- pay-the-bill/script.js
+++ pay-the-bill/script.js
@@ -2,14 +2,10 @@ let tva = document.getElementById("tva");
 let people = document.getElementById("people");
 let result = document.getElementById("result");
 
-console.log(price)
-
 function send(){
- calcTVA();
-}
 
-function calcTVA(){
     let totalTVA = price.value * tva.value / 100
     let total = price.value * 1 + totalTVA * 1
-    result.textContent = "Le total est de : " + total + " euros"
+    let totalPeople = total / people.value
+    result.textContent = "Le coût est de " + totalPeople + " euros par personne."
 }
