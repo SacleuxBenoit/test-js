40a9aa8f135031ced8d7e48d2b95ce10f50e62cf
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-08-05 19:44:42 +0200

    add function for each button, and 2 variable lifeP1 and lifeP2 = 0 in script.js

--- game-1v1-2D/script.js
+++ game-1v1-2D/script.js
@@ -1,5 +1,24 @@
-let pseudo = document.getElementById("pseudo");
+let lifeP1 = 0;
+let lifeP2 = 0;
 
 function redirection(){
     document.location.href="Game/game.html"
+}
+
+function quickAttack(){
+    let min = 6;
+    let max = 12;
+    console.log(Math.floor((Math.random() * (max - min) + min)))
+}
+
+function slowAttack(){
+    let min = 1;
+    let max = 20;
+    console.log(Math.floor((Math.random() * (max - min) + min)))
+}
+
+function heal(){
+    let min = 8;
+    let max = 11;
+    console.log(Math.floor((Math.random() * (max - min) + min)))
 }
