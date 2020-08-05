e7713cc7d6c58eb6a32c45a2ca47cb5199034453
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-08-05 23:38:05 +0200

    refactor firstPlayer.js

--- game-1v1-2D/js/firstPlayer.js
+++ game-1v1-2D/js/firstPlayer.js
@@ -1,40 +1,37 @@
-let showLifeP1 = document.getElementById("showLifeP1")
-let showLifeP2 = document.getElementById("showLifeP2")
+let showLifeP1 = document.getElementById("showLifeP1");
+let showLifeP2 = document.getElementById("showLifeP2");
 
 let lifeP1 = 100;
 let lifeP2 = 100;
 
 function redirection(){
-    document.location.href="Game/game.html"
+    document.location.href="Game/game.html";
 }
 
 function quickAttackP1(){
-    let min = 6;
-    let max = 12;
-    let random = Math.floor((Math.random() * (max - min) + min))
-    let currentLife = lifeP1 -= random
-    showLifeP1.textContent = currentLife + " %"
-    console.log(currentLife)
-
+    reduceLife(6, 12);
 }
 
 function slowAttackP1(){
-    let min = 1;
-    let max = 20;
-    let random = Math.floor((Math.random() * (max - min) + min))
-    let currentLife = lifeP1 -= random
-    showLifeP1.textContent = currentLife + " %"
-    console.log(currentLife)
-
+    reduceLife(1, 20);
 }
 
 function healP1(){
     let min = 7;
     let max = 10;
-    let random = Math.floor((Math.random() * (max - min) + min))
-    let currentLife = lifeP1 += random
-    showLifeP1.textContent = currentLife + " %"
-    console.log(currentLife)
+    let random = Math.floor((Math.random() * (max - min) + min));
+    let currentLife = lifeP1 += random;
+    showLifeP1.textContent = currentLife + " %";
+    console.log(currentLife);
+}
+
+
+function reduceLife(min, max){
+    let random = Math.floor((Math.random() * (max - min) + min));
+    let currentLife = lifeP1 -= random;
+    showLifeP1.textContent = currentLife + " %";
+    console.log(currentLife);
+
 }
 
 showLifeP1.textContent = lifeP1 + "%";
