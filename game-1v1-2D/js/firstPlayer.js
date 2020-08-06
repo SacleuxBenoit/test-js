e55308b80d350d9a1599bf423bbc45741e74b4c8
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-08-06 10:47:13 +0200

    split the code into another file (script.js)

--- game-1v1-2D/js/firstPlayer.js
+++ game-1v1-2D/js/firstPlayer.js
@@ -1,19 +1,17 @@
-let showLifeP1 = document.getElementById("showLifeP1");
-let showLifeP2 = document.getElementById("showLifeP2");
+function reduceLifeP1(min, max){
+    let random = Math.floor((Math.random() * (max - min) + min));
+    let currentLife = lifeP1 -= random;
+    showLifeP1.textContent = currentLife + " %";
+    console.log(currentLife);
 
-let lifeP1 = 100;
-let lifeP2 = 100;
-
-function redirection(){
-    document.location.href="Game/game.html";
 }
 
 function quickAttackP1(){
-    reduceLife(6, 12);
+    reduceLifeP1(6, 12);
 }
 
 function slowAttackP1(){
-    reduceLife(1, 20);
+    reduceLifeP1(1, 20);
 }
 
 function healP1(){
@@ -25,14 +23,3 @@ function healP1(){
     console.log(currentLife);
 }
 
-
-function reduceLife(min, max){
-    let random = Math.floor((Math.random() * (max - min) + min));
-    let currentLife = lifeP1 -= random;
-    showLifeP1.textContent = currentLife + " %";
-    console.log(currentLife);
-
-}
-
-showLifeP1.textContent = lifeP1 + "%";
-showLifeP2.textContent = lifeP2 + "%";
