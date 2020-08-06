6a2f8f75464bdedcc695b73b8f267ef2f1d781f2
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-08-06 10:47:33 +0200

    solve a problem in secondPlayer.js

--- game-1v1-2D/js/secondPlayer.js
+++ game-1v1-2D/js/secondPlayer.js
@@ -1,16 +1,24 @@
+function reduceLifeP2(min, max){
+    let random = Math.floor((Math.random() * (max - min) + min));
+    let currentLife = lifeP2 -= random;
+    showLifeP2.textContent = currentLife + " %";
+    console.log(currentLife);
+
+}
+
 function quickAttackP2(){
-    reduceLife(6, 12);
+    reduceLifeP2(6, 12);
 }
 
 function slowAttackP2(){
-    reduceLife(1, 20);
+    reduceLifeP2(1, 20);
 }
 
 function healP2(){
     let min = 7;
     let max = 10;
     let random = Math.floor((Math.random() * (max - min) + min));
-    let currentLife = lifeP1 += random;
-    showLifeP1.textContent = currentLife + " %";
+    let currentLife = lifeP2 += random;
+    showLifeP2.textContent = currentLife + " %";
     console.log(currentLife);
 }
