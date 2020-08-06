663bb658edfb86074644a2ffd3396351ae3a412e
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-08-06 16:29:30 +0200

    change the background to (greenyellow) for the function reset() in script.js

--- game-1v1-2D/js/script.js
+++ game-1v1-2D/js/script.js
@@ -10,26 +10,29 @@ function redirection(){
     document.location.href="Game/game.html";
 }
 
-showLifeP1.textContent = lifeP1 + "%";
-showLifeP2.textContent = lifeP2 + "%";
+showLifeP1.textContent = lifeP1 + " %";
+showLifeP2.textContent = lifeP2 + " %";
 
 function resetP1(){
     disabledQuickAttackP1.disabled = false;
     disabledSlowAttackP1.disabled = false;
     disabledHealP1.disabled = false;
-    lifeP2 = 100
-    showLifeP2.textContent = 100 + " %"
+    lifeP2 = 100;
+    showLifeP2.textContent = 100 + " %";
+    showLifeP2.style.background = "greenyellow";
 }
 
 function resetP2(){
     disabledQuickAttackP2.disabled = false;
     disabledSlowAttackP2.disabled = false;
     disabledHealP2.disabled = false;
-    lifeP1 = 100
-    showLifeP1.textContent = 100 + " %"
+    lifeP1 = 100;
+    showLifeP1.textContent = 100 + " %";
+    showLifeP1.style.background = "greenyellow"
 }
 
 function reset(){
     resetP1();
     resetP2();
+    result.textContent = " "
 }
