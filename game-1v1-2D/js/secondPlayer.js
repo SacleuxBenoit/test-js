acc6f89ee6870d9aaa55ed0227f6d07c032ba61b
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-08-06 11:37:58 +0200

    disabled the buttons if currentLife <= 0 for the P1 in secondPlayer.js

--- game-1v1-2D/js/secondPlayer.js
+++ game-1v1-2D/js/secondPlayer.js
@@ -1,9 +1,18 @@
+let disabledQuickAttackP2 = document.getElementById("disabledQuickAttackP2");
+let disabledSlowAttackP2 = document.getElementById("disabledSlowAttackP2");
+let disabledHealP2 = document.getElementById("disabledHealP2");
+
 function reduceLifeP2(min, max){
     let random = Math.floor((Math.random() * (max - min) + min));
-    let currentLife = lifeP2 -= random;
-    showLifeP2.textContent = currentLife + " %";
-    console.log(currentLife);
+    let currentLife = lifeP1 -= random;
+    showLifeP1.textContent = currentLife + " %";
 
+    if(currentLife <= 0){
+        result.textContent = "Le joueur 2 à gagné.";
+        disabledQuickAttackP2.disabled = true;
+        disabledSlowAttackP2.disabled = true;
+        disabledHealP2.disabled = true;
+    }
 }
 
 function quickAttackP2(){
