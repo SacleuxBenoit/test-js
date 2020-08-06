b0e02a4863b9793d5f98db874e25948e7a7c9ab7
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-08-06 14:43:58 +0200

    add the function reset() in script.js

--- game-1v1-2D/js/script.js
+++ game-1v1-2D/js/script.js
@@ -1,5 +1,7 @@
 let showLifeP1 = document.getElementById("showLifeP1");
 let showLifeP2 = document.getElementById("showLifeP2");
+let result = document.getElementById("result");
+
 
 let lifeP1 = 100;
 let lifeP2 = 100;
@@ -10,3 +12,24 @@ function redirection(){
 
 showLifeP1.textContent = lifeP1 + "%";
 showLifeP2.textContent = lifeP2 + "%";
+
+function resetP1(){
+    disabledQuickAttackP1.disabled = false;
+    disabledSlowAttackP1.disabled = false;
+    disabledHealP1.disabled = false;
+    lifeP2 = 100
+    showLifeP2.textContent = 100 + " %"
+}
+
+function resetP2(){
+    disabledQuickAttackP2.disabled = false;
+    disabledSlowAttackP2.disabled = false;
+    disabledHealP2.disabled = false;
+    lifeP1 = 100
+    showLifeP1.textContent = 100 + " %"
+}
+
+function reset(){
+    resetP1();
+    resetP2();
+}
