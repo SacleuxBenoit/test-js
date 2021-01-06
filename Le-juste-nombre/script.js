fe32784c4a416ab6342ef335fc94c2c651d973ee
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-01-06 18:23:46 +0100

    create the function showResult, with a verification if  the current number is > || < || == than the random number

--- Le-juste-nombre/script.js
+++ Le-juste-nombre/script.js
@@ -23,3 +23,19 @@ else if (answer == random){
 }
 
 }*/
+
+let number = document.getElementById('number').value;
+let result = document.getElementById('result');
+let random = Math.floor(Math.random() * 1000);
+
+function showResult(){
+    if(number > random){
+        result.textContent = "Votre nombre est supérieur"
+    }else if(number < random){
+        result.textContent = "Votre nombre est inférieur"
+    }else if(number == random){
+        result.textContent = "Vous avez gagné !"
+    }else{
+        result.textContent = "last"
+    }
+}
