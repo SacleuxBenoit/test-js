14acd789368682da76d5fe572cbf8d075e1c0fa2
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-01-06 18:26:14 +0100

    delete the old code :arrow_right: script.js

--- Le-juste-nombre/script.js
+++ Le-juste-nombre/script.js
@@ -1,29 +1,3 @@
-/*let random = Math.floor(Math.random() * 1000);
-console.log(random)
-
-let question = "Entrez votre Number"
-let answer;
-let i = 0;
-
-while(answer != random){
-answer = prompt(question);
-
-if (answer < random) {
-    alert("Votre nombres est inférieur");
-    i++
-}
-
-else if(answer > random){
-    alert("Votre nombre est supérieur");
-    i++
-}
-
-else if (answer == random){
-    alert("Vous avez gagné en " + i + " coups")
-}
-
-}*/
-
 let number = document.getElementById('number').value;
 let result = document.getElementById('result');
 let random = Math.floor(Math.random() * 1000);
