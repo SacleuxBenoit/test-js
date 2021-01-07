4653bcf07a609a09007aee266f937595f6f5147f
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-01-07 19:08:25 +0100

    delete the last (else if) in the funtion showResult() and create the variable i : incrementation ++ until the player find the good number :arrow_right: script.js

--- Le-juste-nombre/script.js
+++ Le-juste-nombre/script.js
@@ -3,13 +3,14 @@ let result = document.getElementById('result');
 let random = Math.floor(Math.random() * 1000);
 
 function showResult(){
+    let i = 0;
     if(number > random){
-        result.textContent = "Votre nombre est supérieur"
+        result.textContent = "Votre nombre est supérieur";
+        i++;
     }else if(number < random){
-        result.textContent = "Votre nombre est inférieur"
+        result.textContent = "Votre nombre est inférieur";
+        i++;
     }else if(number == random){
-        result.textContent = "Vous avez gagné !"
-    }else{
-        result.textContent = "last"
+        result.textContent = "Vous avez gagné en " + i + " coups";
     }
 }
