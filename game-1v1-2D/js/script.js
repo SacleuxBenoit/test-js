ea6f24baf0d56bfcbdac9c605898115453fdb7d3
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-01-08 20:09:00 +0100

    display the current mana for both player :arrow_right: script.js

--- game-1v1-2D/js/script.js
+++ game-1v1-2D/js/script.js
@@ -1,10 +1,17 @@
 let showLifeP1 = document.getElementById("showLifeP1");
 let showLifeP2 = document.getElementById("showLifeP2");
+
+let showManaP1 = document.getElementById("showManaP1");
+let showManaP2 = document.getElementById("showManaP2");
+
 let result = document.getElementById("result");
 
 
 let lifeP1 = 100;
 let lifeP2 = 100;
+
+let manaP1 = 100;
+let manaP2 = 100;
 
 function redirection(){
     document.location.href="Game/game.html";
@@ -12,6 +19,9 @@ function redirection(){
 
 showLifeP1.textContent = lifeP1 + " %";
 showLifeP2.textContent = lifeP2 + " %";
+
+showManaP1.textContent = manaP1 + " %";
+showManaP2.textContent = manaP2 + " %";
 
 function resetP1(){
     disabledQuickAttackP1.disabled = false;
